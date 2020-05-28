a7f3556b4c7aafe4f2a6fbf352ad50a1db876d65
Dylan Jhaveri <dylanjhaveri@gmail.com>
2020-05-28 10:31:00 -0700

    add useSwr to fetch live stream details

--- pages/live-stream/[id].js
+++ pages/live-stream/[id].js
@@ -1,3 +1,34 @@
+//
+// 1. use the shared layout
+// 2. make request to the api, get the live stream details (status, stream key, etc)
+// 3. If active, show a preview of the live stream in a video player
+//
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import useSWR from 'swr'
+import Layout from '../../components/layout'
+import request from '../../components/request'
+import Spinner from '../../components/spinner'
+import ErrorMessage from '../../components/error-message'
+
+const fetcher = (url) => request(url, {method: 'GET'})
+
 export default () => {
-  return <div>Live stream page</div>
+  const router = useRouter()
+  const { id } = router.query
+  console.log('rendering', id)
+  const { data, error } = useSWR(() =>
+    id ? `/api/live-stream/${id}` : null
+  , fetcher)
+
+  console.log('debug', error)
+  if (error) {
+    return <Layout title="Live stream page"><ErrorMessage message={error.message} /></Layout>
+  }
+  console.log('debug', data, error)
+  return (
+    <Layout title="Live stream page">
+      <div>Live stream page {id}</div>
+    </Layout>
+  )
 }
