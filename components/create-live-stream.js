4e4ef662e60c9a96133459678451decc7ba286c9
Dylan Jhaveri <dylanjhaveri@gmail.com>
2020-05-28 09:58:42 -0700

    add a spiner for create live stream button

--- components/create-live-stream.js
+++ components/create-live-stream.js
@@ -1,11 +1,19 @@
+import { useState } from 'react'
 import Button from './button'
+import Spinner from './spinner'
 
 export default () => {
+  const [isCreating, setIsCreating] = useState(false)
+
   const createLiveStream = () => {
+    setIsCreating(true)
+    // make an api request to the backend
+    // backend talks to Mux
+    // get a response
     console.log('debug createLiveStream click')
   }
 
-  return (
-    <Button onClick={createLiveStream}>Create live stream</Button>
-  )
+  return isCreating ?
+  <Spinner /> :
+  <Button onClick={createLiveStream}>Create live stream</Button>
 }
