8fd0cea919e2e7f51b4b58f90d9ce91ad8d74bdb
Dylan Jhaveri <dylanjhaveri@gmail.com>
2020-05-28 10:07:29 -0700

    api request to create live stream and log on the frontend

--- components/create-live-stream.js
+++ components/create-live-stream.js
@@ -1,17 +1,29 @@
 import { useState } from 'react'
 import Button from './button'
 import Spinner from './spinner'
+import ErrorMessage from './error-message'
 
 export default () => {
   const [isCreating, setIsCreating] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(false)
 
-  const createLiveStream = () => {
+  const createLiveStream = async () => {
     setIsCreating(true)
     // make an api request to the backend
     // backend talks to Mux
     // get a response
-    console.log('debug createLiveStream click')
+    // POST /api/live-stream
+    try {
+      let resp = await fetch('/api/live-stream', {method: 'POST'})
+      resp = await resp.json()
+      console.log('debug resp', resp)
+    } catch (e) {
+      console.error('Error creating live stream', e)
+      setErrorMessage('Error creating live stream')
+    }
   }
+
+  if (errorMessage) return <ErrorMessage message={errorMessage} />
 
   return isCreating ?
   <Spinner /> :
